865ad76adea1a73418094453e8e2000fad574ffa
natasha Selvidge <natashaselvidge@natashas-Air.attlocal.net>
2016-05-17 21:05:17 -0700

    init commit

---
+++ src/Automobile.java
@@ -0,0 +1,41 @@
+
+//Automobile.java
+
+public class Automobile {
+  public int year;
+  public String make;
+  public String model;
+  public String color;
+  public String name;
+  public int gears;
+  public int operatingCycle = 0;
+  public int currentGear = 0;
+  public int currentSpeed = 0;
+  public int distanceTraveled = 0;
+
+  
+  public Automobile (int year, String make, String model, String color, String name, int gears){
+    this.year = year;
+    this.make = make;
+    this.model = model;
+    this.color = color;
+    this.name = name;
+    this.gears = gears;
+  }
+  public void increaseCycle(){
+    operatingCycle++;
+    if( currentGear + 1 <= gears ) {
+      currentGear++;
+      currentSpeed = currentGear * 10;
+    }
+    distanceTraveled = distanceTraveled + (currentSpeed/60);
+  }
+  public void decreaseCycle(){
+    operatingCycle--;
+    if( currentGear - 1 > 0 ) {
+      currentGear--;
+      currentSpeed = currentGear * 10;
+    }
+    distanceTraveled = distanceTraveled + (currentSpeed/60);
+  }
+}
---
+++ src/RaceGame.java
@@ -0,0 +1,90 @@
+
+
+import java.util.Scanner;
+
+public class RaceGame {
+  public static void main (String[] args) {
+    int year;
+    String make;
+    String model;
+    String color;
+    String name;
+    int gears;
+    int corners;
+    int laps;
+    int straights;
+    String trackName;
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Hello! lets make your car.");
+    System.out.println("What year is your car?: ");
+    year = in.nextInt();
+    in.nextLine();
+    System.out.println("What make is your car?: ");
+    make = in.nextLine();
+    System.out.println("What model is your car?: ");
+    model = in.nextLine();
+    System.out.println("What color is your car?: ");
+    color = in.nextLine();
+    System.out.println("What is the name of your car?: ");
+    name = in.nextLine();
+    System.out.println("How many gears does your car have?: ");
+    gears = in.nextInt();
+    in.nextLine();
+
+    System.out.println("Great! Heres the info about your car: ");
+    Automobile userCar = new Automobile(year, make, model, color, name, gears);
+
+    System.out.println("year: " + userCar.year);
+    System.out.println("Make: " + userCar.make);
+    System.out.println("Model: " + userCar.model);
+    System.out.println("Color: " + userCar.color);
+    System.out.println("Name: " + userCar.name);
+    System.out.println("Gears: " + userCar.gears);
+
+    System.out.println("Now let's pick your track! How many corners should be on your track? (max 10): ");
+    corners = in.nextInt();
+    in.nextLine();
+    straights = 10 - corners;
+    System.out.println("How many laps will you go on your track?: ");
+    laps = in.nextInt();
+    in.nextLine();
+    System.out.println("What will you name your track?: ");
+    trackName = in.nextLine();
+
+    int lapNumber = 0;
+    Double yesOrNo;
+    while(lapNumber < laps){
+      System.out.println("Starting a new lap!");
+      int lapPortionNumber = 0;
+      int lapCorners = corners;
+      int lapStraights = straights;
+      while(lapPortionNumber < 10){
+        if(lapCorners > 0 && lapStraights > 0){
+          yesOrNo = Math.random();
+          if(yesOrNo < 0.5){
+            System.out.println("Hitting a Corner!");
+            userCar.decreaseCycle();
+            lapCorners = lapCorners - 1;
+          } else {
+            System.out.println("Hitting a Straight!");
+            userCar.increaseCycle();
+            lapStraights = lapStraights - 1;
+          }
+
+        } else if (lapCorners > 0) {
+          System.out.println("Hitting a Corner!");
+          userCar.decreaseCycle();
+          lapCorners = lapCorners - 1;
+        } else {
+          System.out.println("Hitting a Straight!");
+          userCar.increaseCycle();
+          lapStraights = lapStraights - 1;
+        }
+        lapPortionNumber++;
+      }
+      lapNumber++;
+    }
+    System.out.println("Race Complete! Your car traveled: " + userCar.distanceTraveled);
+  }
+}
